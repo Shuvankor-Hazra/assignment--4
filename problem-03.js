309607dcc04faac8f56f6c07d8f41a648b3bab73
Shuvankr Hazra <shuvo.s.ovuhs@gmail.com>
2024-02-08 00:49:55 +0600

    problem 3 added

---
+++ problem-03.js
@@ -0,0 +1,13 @@
+function deleteInvalids(array) {
+  if (!Array.isArray(array)) {
+    return "Input should be an array!";
+  }
+  let newArray = [];
+  for(const element of array){
+      if(typeof element === "number" && !isNaN(element)){
+        newArray.push(element);
+    }
+  }
+  return newArray;
+}
+console.log(deleteInvalids({num: [ 1 , 2 , 3 ]} ));
